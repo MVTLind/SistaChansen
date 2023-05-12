c5606dc5cec80fdf735d1f51dec8abc1b6002c6d
MVTLind <ebba.lindskog@gmail.com>
2023-05-12 20:30:59 +0200

    Har lagt till grundläggande setup för selenium i VS Code

---
+++ Setup.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+import time
+chromedriver = "C:\Program Files (x86)\chromedriver.exe"
+s = Service(chromedriver)
+driver = webdriver.Chrome(service=s)
+
+driver.get("https://www.sfbok.se/")
+print(driver.title)
+driver.quit()
